90b978f50ffc316e80b230a9e862947b50e1e8b9
Moubarek Barre Hassan <69253659+MOUBAREK1998@users.noreply.github.com>
2021-05-06 10:50:21 +0300

    Update HW_task1.c

--- MOUBAREK BARRE HASSAN HW-1/HW_task1.c
+++ MOUBAREK BARRE HASSAN HW-1/HW_task1.c
@@ -1,83 +1,42 @@
 // Task1 Linear structure of processes: P1 -> P2 -> P3 + P4
 
-// Task1 Linear structure of processes: P1 -> P2 -> P3 + P4
-
+#include <stdio.h> 
 #include <unistd.h>
-#include <stdio.h>
 
-int main(void) {
-	pid_t pid, pid1, pid2, pid_temp, pid_temp1, pid_temp2, pid_temp3; 
-	pid_t ppid, ppid1, ppid2, ppid_temp, ppid_temp1, ppid_temp2, ppid_temp3;
+
+
+int main() 
+{ 
+    pid_t pid, pid_3, pid_4;
+    int status, status2, status3;
+    printf(" Parent Parent  id: %d\n", getpid()); 
+
+    pid = fork();
+    wait(&status);
+
+    if (pid == 0) {    
+        printf("child1  id: %d\n", getpid()); 
+        printf("  Parent child1 id: %d\n", getppid());
+
+        pid_3 = fork();
+	
+	wait(&status2);
 	
 
-	printf("Parent start\n");
-	pid_temp = getpid();
-	ppid_temp = getppid();
-	printf(" Parent PID :: %i\n",pid_temp);
-	printf(" Parent parent PID :: %i\n",ppid_temp);
-
-	pid = fork();
-
-	if ( pid < 0 ) {        
-		return 1;
-	}
-	else if ( pid == 0 ) {
-		// in Child1
-		printf("\tChild1 start\n");
-		pid_temp1 = getpid();
-		ppid_temp1 = getppid();
-		printf("\t Child1 PID :: %i\n",pid_temp1);
-		printf("\t Child1 parent PID :: %i\n",ppid_temp1);
-		pid1 = fork();
-        pid2 = fork();
-		if ( pid1 < 0 ) {        
-			return 1;
-		}
-		else if ( pid1 == 0 ) {
-			// in Child2
-			printf("\t\tChild2 start\n");
-			pid_temp2 = getpid();
-			ppid_temp2 = getppid();
-			printf("\t\t Child2 PID :: %i\n",pid_temp2);
-			printf("\t\t Child2 parent PID :: %i\n",ppid_temp2);
-			
+        if (pid_3 == 0){
+            printf("child2 id: %d\n", getpid()); 
+            printf("Parent chid2 id: %d\n", getppid()); 
+	    return 0;
         }
-		else {
-				// in Child2
-				// Waiting for Child3       
-				
-				printf("\t\tChild2 stopped\n");
-			}
-			return 0; // for stops Child2
-		
-        if ( pid2 < 0 ) {        
-				return 1;
-			}
-		else if ( pid2 == 0 ) {
-				// in Child3
-				printf("\t\t\tChild3 start\n");
-				pid_temp3 = getpid();
-				ppid_temp3 = getppid();
-				printf("\t\t\t Child3 PID :: %i\n",pid_temp3);
-				printf("\t\t\t Child3 parent PID :: %i\n",ppid_temp3);
-				sleep(2);        
-				printf("\t\t\tChild3 stopped\n");
-				return 0; // for stops Child3
-			}
-    
-		else {
-			// in Child1
-			// Waiting for Child2       
-			
-			printf("\tChild1 stopped\n");
-		}        
-		return 0; // for stops Child1
+	
+	pid_4 = fork();
+	wait(&status3);
+
+	if (pid_4 == 0){
+	    printf("child3 id: %d\n", getpid());
+	    printf("Parent child3 id: %d\n", getppid());
 	}
-	else {
-		// Parent process
-		// Waiting for Child1       
-		
-		printf("Parent stopped\n");
-	} 
-	return 0; // for stops Parent
-}
+    }
+
+    exit(0);
+} 
